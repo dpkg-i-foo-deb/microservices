25b3d8c430c809e09aaa53c82321ff68ca13c6f3
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2023-08-12 16:23:54 -0500

    Servidor

--- taller-1/reto-3/servidor/src/main.rs
+++ taller-1/reto-3/servidor/src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+mod models;
+mod handlers;
+mod services;
+
+#[macro_use] extern crate rocket;
+#[launch]
+fn rocket() ->_{
+    rocket::build().mount("/", routes![handlers::greet::greet])
 }
---
+++ taller-1/reto-3/servidor/src/services/greet.rs
@@ -0,0 +1,3 @@
+pub fn build_greet(username:String)-> String{
+    format!("Holis! {}",username)
+}
