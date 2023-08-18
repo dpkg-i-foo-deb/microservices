56ded2726a99fdc9a3d460e0a02cfbc40eb6856a
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2023-08-18 15:40:18 -0500

    Script

--- taller-2/reto-1/client/script.sh
+++ taller-2/reto-1/client/script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 surreal isready --conn http://contenedor-bd-reto-1:8000
 
-ls
+cat data/0-db.surql | surreal sql --conn http://contenedor-bd-reto-1:8000 --user root --pass root 
 
 surreal import --conn http://contenedor-bd-reto-1:8000 --user root --pass root --db auth --ns auth data/0-db.surql
