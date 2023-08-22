96e863b46efa616dbd728923e6aa7b318224cb20
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2023-08-22 18:07:38 -0500

    Whitespace

--- taller-2/reto-1/client/script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-surreal isready --conn http://contenedor-bd-reto-1:8000
-
-cat data/0-db.surql | surreal sql --conn http://contenedor-bd-reto-1:8000 --user root --pass root 
-
-surreal import --conn http://contenedor-bd-reto-1:8000 --user root --pass root --db auth --ns auth data/1-model.surql
