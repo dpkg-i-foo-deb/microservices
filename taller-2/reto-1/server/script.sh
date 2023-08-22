2414e038cd01a64a64f49a8a0f7dae30a0ecfc7c
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2023-08-22 18:47:32 -0500

    Script

--- taller-2/reto-1/server/script.sh
+++ taller-2/reto-1/server/script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-surreal start --log debug --user root --pass root file:database &
+surreal start --log debug --user root --pass root file:database/surreal.db &
 surreal isready  --conn http://localhost:8000
 cat data/0-db.surql | surreal sql --conn http://localhost:8000 --user root --pass root
 surreal import --conn http://localhost:8000 --user root --pass root --db auth --ns auth data/1-model.surql
