b3729af5cb96b57870434f075d8054118bd1fd7c
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2023-08-22 19:33:55 -0500

    Script

--- taller-2/reto-1/server/script.sh
+++ taller-2/reto-1/server/script.sh
@@ -3,7 +3,11 @@
 
 if [ -d "database" ] 
 
+    surreal start --log debug --user root --pass root file:database/surreal.db
+
 then 
+
+else
 
     surreal start --log debug --user root --pass root file:database/surreal.db &
 
@@ -13,10 +17,6 @@ then
     surreal isready  --conn http://localhost:8000
     cat data/0-db.surql | surreal sql --conn http://localhost:8000 --user root --pass root
     surreal import --conn http://localhost:8000 --user root --pass root --db auth --ns auth data/1-model.surql
-
-else
-
-    surreal start --log debug --user root --pass root file:database/surreal.db
 
 fi
 
