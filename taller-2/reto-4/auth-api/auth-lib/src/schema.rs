cc046b5cee4e989804ca9f721f9f829fcac8bdd6
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2023-08-28 11:08:02 -0500

    Diesel schema

---
+++ taller-2/reto-4/auth-api/auth-lib/src/schema.rs
@@ -0,0 +1,31 @@
+// @generated automatically by Diesel CLI.
+
+diesel::table! {
+    sessions (id) {
+        #[max_length = 255]
+        id -> Varchar,
+        start_date -> Timestamp,
+        end_date -> Nullable<Timestamp>,
+        #[max_length = 255]
+        user_id -> Varchar,
+    }
+}
+
+diesel::table! {
+    users (id) {
+        #[max_length = 255]
+        id -> Varchar,
+        #[max_length = 255]
+        email -> Varchar,
+        #[max_length = 255]
+        password -> Varchar,
+        #[max_length = 255]
+        username -> Varchar,
+        #[max_length = 255]
+        status -> Varchar,
+    }
+}
+
+diesel::joinable!(sessions -> users (user_id));
+
+diesel::allow_tables_to_appear_in_same_query!(sessions, users,);
